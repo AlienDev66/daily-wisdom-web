2ba43dcb50eabb5068cf14ce6582dadb1b02b2c2
Domingos Capitango <p.capitango65@gmail.com>
2024-11-26 00:36:51 +0000

    :feature: add header logo and border radius

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,22 @@
+import Image from "next/image";
 import Link from "next/link";
 
 const Header: React.FC = () => {
   return (
     <header className="bg-gray-800 text-white p-4">
       <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-xl font-bold">Daily Wisdom</h1>
+        <div className="flex items-center gap-3">
+          <Image
+            className="dark:invert rounded-lg"
+            src="/logo.png"
+            alt="Daily Wisdom logo"
+            width={50}
+            height={38}
+            priority
+          />
+          <h1 className="text-xl font-bold">Daily Wisdom</h1>
+        </div>
+
         <nav>
           <Link href="/" className="text-gray-300 hover:text-white px-3">
             Home
